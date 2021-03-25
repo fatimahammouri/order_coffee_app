719b351f9b71c60466a9d60fa1b6b3870ac9f4dc
Fatima <fatima.hammouri@gmail.com>
2021-03-25 12:26:10 +0000

    initial commit

---
+++ static/js/coffeeShop.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const addItemToCart = (itemName) => {
+  $('#cart-items').append(`
+    <tr>
+      <td>${itemName}</td>
+    </tr>a
+  `);
+  
+};
+const addToCartHandler = (evt)=>{
+  const buttonElement = evt.target;
+  addItemToCart(buttonElement.getAttribute('product')); 
+  
+};
+
+const resetCart = () => {
+  $('#cart-total').html('0.00');
+  $('#cart-items').empty();
+};
+
+const incrementCartTotal = (price) => {
+  const cartTotal = $('#cart-total');
+
+  let total = Number(cartTotal.html());
+  total += price;
+
+  cartTotal.html(total.toFixed(2));
+};
+
+const incrementCoffeeSold = (amountSold) => {
+  let coffeeSold = Number($('#coffee-sold-counter').html());
+  coffeeSold += amountSold;
+
+  $('#coffee-sold-counter').html(coffeeSold);
+};
+
+const setProgressAndStatus = (progressVal, statusMsg) => {
+  $('#order-progress').attr('value', progressVal);
+  $('#order-status-message').html(statusMsg);
+};
+
+
+//
+// Add your event handlers below.
+//
+
